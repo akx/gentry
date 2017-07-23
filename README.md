c55f574e2639b0d2c41e0515ee613908c7d259d0
Aarni Koskela <akx@iki.fi>
2017-07-23 17:19:46 +0300

    Init

---
+++ README.md
@@ -0,0 +1,8 @@
+Gentry
+======
+
+A splendidly simple implementation of a [Raven][raven] receiver.
+
+For small-time users.
+
+[raven]: https://docs.sentry.io/clients/python/
---
+++ gentry/settings.py
@@ -0,0 +1,65 @@
+import os
+from environ import Env
+env = Env()
+DEBUG = env.bool('DEBUG', default=False)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+SECRET_KEY = env.str('SECRET_KEY', default=('x' if DEBUG else Env.NOTSET))
+ALLOWED_HOSTS = ['*']
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+STATIC_URL = '/static/'
+WSGI_APPLICATION = 'gentry.wsgi.application'
+ROOT_URLCONF = 'gentry.urls'
+LOGIN_URL = '/admin/login/'
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+DATABASES = {
+    'default': env.db_url('DATABASE_URL', default='sqlite:///%s' % os.path.join(BASE_DIR, 'db.sqlite3')),
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+]
+if DEBUG:
+    AUTH_PASSWORD_VALIDATORS = []
