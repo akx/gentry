c4b6be0bbbf20aeac42e4cab0737591772d0ea16
Aarni Koskela <akx@iki.fi>
2017-08-02 11:28:15 +0300

    Gontend: Wire up redux-thunk for async

--- gontend/src/index.jsx
+++ gontend/src/index.jsx
@@ -3,15 +3,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {HashRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
-import {createStore, compose} from 'redux';
+import {createStore, compose, applyMiddleware} from 'redux';
 import persistState from 'redux-localstorage';
-import rootReducer from './reducers';
+import thunk from 'redux-thunk';
 
+import rootReducer from './reducers';
 import App from './App';
 import style from './style/style.less';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;  // eslint-disable-line
-const enhancer = composeEnhancers(persistState(null, 'gontend'));
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk),
+  persistState(null, 'gontend')
+);
 const store = createStore(rootReducer, enhancer);
 
 window.addEventListener('load', () => {
