5dfef34af9f780ba26f96078347ff86163c17d1f
Aarni Koskela <akx@iki.fi>
2017-08-02 11:29:45 +0300

    Gontend: Add archival button

--- gontend/src/actions/index.js
+++ gontend/src/actions/index.js
@@ -1,3 +1,11 @@
-/* eslint-disable import/prefer-default-export */
+/* eslint-env browser */
 export const updateSearchParams = values => ({type: 'updateSearchParams', payload: values});
 export const resetSearchParams = () => ({type: 'resetSearchParams'});
+export const archiveEvent = eventId => (dispatch) => {
+  dispatch({type: 'archiveEventStart', payload: {eventId}});
+  fetch(`/api/event/${eventId}/archive/`, {method: 'POST', credentials: 'same-origin'})
+    .then(r => r.json())
+    .then(() => {
+      dispatch({type: 'archiveEvent', payload: {eventId}});
+    });
+};
