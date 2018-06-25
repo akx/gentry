0ed69f1acd0cd9b8c709df9f35424271ec5a89a0
Aarni Koskela <akx@iki.fi>
2018-06-25 20:21:36 +0300

    API: test projects API too

--- gore/api/handlers/projects.py
+++ gore/api/handlers/projects.py
@@ -1,7 +1,9 @@
+from gore.api.schemata import ProjectSchema
 from gore.api.utils import check_authenticated
 from gore.models import Project
 
 
 def get_project_list(request):
     check_authenticated(request)
-    return list(Project.objects.all().values('id', 'name'))
+    projects = Project.objects.all()
+    return list(ProjectSchema().dump(projects, many=True).data)
---
+++ gore/tests/test_projects_api.py
@@ -0,0 +1,17 @@
+import json
+
+import pytest
+from django.utils.encoding import force_text
+
+
+@pytest.mark.django_db
+def test_projects_api(project, admin_client):
+    list_resp = json.loads(force_text(admin_client.get('/api/projects/').content))
+    assert len(list_resp) == 1
+    proj_obj = list_resp[0]
+    assert set(proj_obj.keys()) == {'id', 'name', 'slug'}
+    assert proj_obj['id'] == project.id
+
+
+def test_projects_api_auth(client):
+    assert client.get('/api/projects/').status_code >= 400
